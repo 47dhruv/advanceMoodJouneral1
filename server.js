282e988a08161d5957587d921dcec56af3fb91b3
47dhruv <dhruvpandey744@gmail.com>
2026-05-02 21:53:28 +0530

    add frontend env with backend URL

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/assignment1";
 
-app.use(cors());
+app.use(cors({
+  origin: "https://advance-mood-jouneral1.vercel.app"
+}));
 app.use(express.json({ limit: "1mb" }));
 
 app.get("/api/health", (req, res) => {
